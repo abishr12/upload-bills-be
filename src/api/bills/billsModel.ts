f160c0afddce103d073f0e119df02b6ea8ee4ab7
abishr <adhambishr@gmail.com>
2024-11-05 13:31:03 -0800

    fix for schema

--- src/api/bills/billsModel.ts
+++ src/api/bills/billsModel.ts
@@ -1,7 +1,7 @@
 import { extendZodWithOpenApi } from "@asteasolutions/zod-to-openapi";
 import { z } from "zod";
 
-import { commonValidations } from "@/common/utils/commonValidation";
+extendZodWithOpenApi(z);
 
 export type Bill = z.infer<typeof BillsSchema>;
 export const BillsSchema = z.object({
